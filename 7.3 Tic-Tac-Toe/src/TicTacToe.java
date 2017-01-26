d16a05132d493b34b3ebe7a41ac4611ce0566066
531931 <531931@6RM16-30.cuhsd-int.prv>
2017-01-26 11:55:21 -0800

    more stuff

--- 7.3 Tic-Tac-Toe/src/TicTacToe.java
+++ 7.3 Tic-Tac-Toe/src/TicTacToe.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.IndexOutOfBoundsException;
 
 public class TicTacToe extends PlayTicTacToe
 {
@@ -23,9 +24,11 @@ public class TicTacToe extends PlayTicTacToe
 	}
 	
 
-	private void playerMove(int player)
+	private void playerMove(int player) 
 	{
 		String marker = null;
+		int row = 0;
+		int column = 0;
 		if(player == 1)
 		{
 			System.out.println("Player One, please place an X");
@@ -44,45 +47,64 @@ public class TicTacToe extends PlayTicTacToe
 		if(move.equals("UR") && gameBoard[0][2].isEmpty())
 		{
 			gameBoard[0][2] = marker;
+			row = 0;
+			column = 2;
 		}
 		else if(move.equals("UC") && gameBoard[0][1].isEmpty())
 		{
 			gameBoard[0][1] = marker;
+			row = 0; 
+			column = 2;
 		}
 		else if(move.equals("UL") && gameBoard[0][0].isEmpty())
 		{
 			gameBoard[0][0] = marker;
+			row = 0;
+			column = 0;
 		}
 		else if(move.equals("CR") && gameBoard[1][2].isEmpty())
 		{
 			gameBoard[1][2] = marker;
+			row = 1;
+			column = 2;
 		}
 		else if(move.equals("CC") && gameBoard[1][1].isEmpty())
 		{
 			gameBoard[1][1] = marker;
+			row = 1;
+			column = 1;
 		}
 		else if(move.equals("CL") && gameBoard[1][0].isEmpty())
 		{
 			gameBoard[1][0] = marker;
+			row = 1;
+			column = 0;
 		}
 		else if(move.equals("LR") && gameBoard[2][2].isEmpty())
 		{
 			gameBoard[2][2] = marker;
+			row = 2;
+			column = 2;
 		}
 		else if(move.equals("LC") && gameBoard[2][1].isEmpty())
 		{
 			gameBoard[2][1] = marker;
+			row = 2;
+			column = 1;
 		}
 		else if(move.equals("LL") && gameBoard[2][2].isEmpty())
 		{
 			gameBoard[2][2] = marker;
+			row = 2;
+			column = 2;
 		}
 		else
 		{
 			System.out.println("Invalid input, your turn is skipped.");
 		}
 		printBoard();
-		checkIfWon(marker, move);
+		checkIfWon(marker, row, column);
+		
 	}
 	
 	public void printBoard()
@@ -98,8 +120,17 @@ public class TicTacToe extends PlayTicTacToe
 		}
 	}
 	
-	private void checkIfWon(String theMarker, String theMove)
+	private void checkIfWon(String theMarker, int theRow, int theColumn)
 	{
-		
+		//check column
+		for(int i = 0; i < 3; i++)
+		{
+			if(gameBoard[i][theColumn].equals(theMarker))
+			{
+				
+			}
+		}
 	}
+	
+
 }
